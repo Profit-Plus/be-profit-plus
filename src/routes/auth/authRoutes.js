c12e0e43dbd199280dbc87f90bf35cfb3472ff79
KristiantoJohan <johankristianto793@gmail.com>
2024-03-25 07:55:57 +0700

    Fixing the authorization feature

--- src/routes/auth/authRoutes.js
+++ src/routes/auth/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const authRouter = express.Router();
-const { registerController, loginController, testController } = require('../../controllers/authController/authController');
+const authController = require('../../controllers/authController/authController');
 const { authenticationMiddleware } = require('../../middlewares/authMiddlewares/authMiddleware');
 
-authRouter.post('/porto/register',  registerController.newUserRegistration);
-authRouter.post('/porto/login',  loginController.existingUserLogin);
-authRouter.get('/porto/dashboard', authenticationMiddleware.authenticateUser,  testController.dashboardController);
+authRouter.post('/porto/register',  authController.registerController);
+authRouter.post('/porto/login',  authController.loginController);
+authRouter.get('/porto/dashboard', authenticationMiddleware.authenticateUser,  authController.dashboardController);
 
 module.exports = authRouter;
