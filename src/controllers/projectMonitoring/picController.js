d1382da4fb5b1766a79a5c719f04c9c614750d80
Fajar Sidiq <fajarsiedd@gmail.com>
2024-04-23 13:43:44 +0700

    feat(validator): Implement error message formatter

--- src/controllers/projectMonitoring/picController.js
+++ src/controllers/projectMonitoring/picController.js
@@ -1,8 +1,10 @@
+const { Prisma } = require('@prisma/client');
 const picService = require('../../services/projectMonitoring/pic.service');
 const webResponses = require('../../helpers/web/webResponses');
 const { v4: uuidv4 } = require('uuid');
 const Ajv = require('ajv');
 const picValidator = require('../../validators/PIC.validator');
+const { formatErrorMessage } = require('../../helpers/utils/validator/formatError')
 
 const ajv = new Ajv();
 
@@ -10,16 +12,11 @@ async function createPIC(req, res) {
     try {
         const createPicValidate = ajv.compile(picValidator.createPIC);
 
-        if (!createPicValidate(req.body)) {     
-            let prop = createPicValidate.errors[0].instancePath;
-            if (prop) prop = '\'' + createPicValidate.errors[0].instancePath.replace('/', '') + '\' ';
-            const errMessage = prop + createPicValidate.errors[0].message;
+        if (!createPicValidate(req.body)) {
+            return res.status(400).json(webResponses.errorResponse('Invalid input! ' + formatErrorMessage(createPicValidate.errors[0])));
+        }
 
-            res.status(400).json(webResponses.errorResponse('Invalid input! '+ errMessage));
-            throw new Error('There are several fields empty!');
-        }        
-
-        const {name, phone, role} = req.body;
+        const { name, phone, role } = req.body;
 
         const id = uuidv4();
 
@@ -33,69 +30,88 @@ async function createPIC(req, res) {
         });
 
         res.status(200).json(webResponses.successResponse('PIC created successfully!', pic));
-    } catch (error) {
-        console.log(error)
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
+                return res.status(400).json(webResponses.errorResponse('There is a unique constraint violation on the constraint \'phone\''));
+            }
+        }
+        throw e;
     }
 }
 
 async function getAllPICs(req, res) {
-    const pic = await picService.findAllPics();
+    try {
+        const pic = await picService.findAllPics();
 
-    if (pic) {
-        res.status(200).json(webResponses.successResponse('PICs data fetched successfully!', pic));
-    }
-    else {
-        res.status(404).json(webResponses.successResponse('PIC not found!'));
+        if (pic) {
+            res.status(200).json(webResponses.successResponse('PICs data fetched successfully!', pic));
+        }
+        else {
+            res.status(404).json(webResponses.successResponse('PIC not found!'));
+        }
+    } catch (e) {
+        console.log(e);
+        throw e;
     }
 }
 
 async function getPIC(req, res) {
-    const picId = req.params.id;
+    try {
+        const picId = req.params.id;
 
-    const pic = await picService.findPic(picId);
+        const pic = await picService.findPic(picId);
 
-    if (pic) {
-        res.status(200).json(webResponses.successResponse('PIC data fetched successfully!', pic));
-    }
-    else {
-        res.status(404).json(webResponses.successResponse('PIC not found!'));
+        if (pic) {
+            res.status(200).json(webResponses.successResponse('PIC data fetched successfully!', pic));
+        }
+        else {
+            res.status(404).json(webResponses.successResponse('PIC not found!'));
+        }
+    } catch (e) {
+        console.log(e)
+        throw e;
     }
 }
 
 async function updatePIC(req, res) {
-    const updatePicValidate = ajv.compile(picValidator.updatePIC);
+    try {
+        const updatePicValidate = ajv.compile(picValidator.updatePIC);
 
-    if (!updatePicValidate(req.body)) {     
-        let prop = updatePicValidate.errors[0].instancePath;
-        if (prop) prop = '\'' + updatePicValidate.errors[0].instancePath.replace('/', '') + '\' ';
-        const errMessage = prop + updatePicValidate.errors[0].message;
+        if (!updatePicValidate(req.body)) {
+            return res.status(400).json(webResponses.errorResponse('Invalid input! ' + formatErrorMessage(updatePicValidate.errros[0])));
+        }
 
-        res.status(400).json(webResponses.errorResponse('Invalid input! '+ errMessage));
-        throw new Error('There are several fields empty!');
-    }        
-    
-    const { name, phone, role } = req.body;
-    
-    const picId = req.params.id;
+        const { name, phone, role } = req.body;
 
-    const pic = await picService.updatePic(picId, {
-        name: name,
-        phone: phone,
-        role: role
-    });
+        const picId = req.params.id;
 
-    if (pic) {
-        res.status(200).json(webResponses.successResponse('PIC updated successfully!', pic));
-    }
-    else {
-        res.status(404).json(webResponses.successResponse('PIC not found!'));
+        const pic = await picService.updatePic(picId, {
+            name: name,
+            phone: phone,
+            role: role
+        });
+
+        if (pic) {
+            res.status(200).json(webResponses.successResponse('PIC updated successfully!', pic));
+        }
+        else {
+            res.status(404).json(webResponses.successResponse('PIC not found!'));
+        }
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
+                return res.status(400).json(webResponses.errorResponse('There is a unique constraint violation on the constraint \'phone\''));
+            }
+        }
+        throw e;
     }
 }
 
 async function deletePIC(req, res) {
     const picId = req.params.id;
 
-    const pic = await picService.findPic(picId);    
+    const pic = await picService.findPic(picId);
 
     if (pic) {
         await picService.deletePic(picId);
