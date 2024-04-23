ca2aedc7ece0a8d76b7da6d882c5f8fe074d29c2
Fajar Sidiq <fajarsiedd@gmail.com>
2024-04-23 13:42:21 +0700

    feat(validator): Update PIC validator

--- src/validators/PIC.validator.js
+++ src/validators/PIC.validator.js
@@ -1,9 +1,9 @@
 const createPIC = {
     type: 'object',
     properties: {
-        name: { type: 'string', minLength: 5, maxLength: 68, nullable: false },
-        phone: { type: 'string', minLength: 5, maxLength: 13, nullable: false },
-        role: { type: 'string', minLength: 5, maxLength: 68, nullable: false},
+        name: { type: 'string', minLength: 1, maxLength: 68, nullable: false },
+        phone: { type: 'string', minLength: 7, maxLength: 13, nullable: false },
+        role: { type: 'string', enum: ["LIRA", "external", "midfielder"], minLength: 1, nullable: false },
     },
     required: ['name', 'phone', 'role'],
     additionalProperties: false,
@@ -12,9 +12,9 @@ const createPIC = {
 const updatePIC = {
     type: 'object',
     properties: {
-        name: { type: 'string', minLength: 5, maxLength: 68, nullable: false },
-        phone: { type: 'string', minLength: 5, maxLength: 13, nullable: false },
-        role: { type: 'string', minLength: 5, maxLength: 68, nullable: false},
+        name: { type: 'string', minLength: 1, maxLength: 68, nullable: false },
+        phone: { type: 'string', minLength: 7, maxLength: 13, nullable: false },
+        role: { type: 'string', enum: ["LIRA", "external", "midfielder"], minLength: 1, nullable: false },
     },
     additionalProperties: false,
 }
