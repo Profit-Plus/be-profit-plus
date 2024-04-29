888464c77f1e839c8dad9e78b2ed0dac6b6ee682
Fajar Sidiq <fajarsiedd@gmail.com>
2024-04-29 17:43:17 +0700

    feat(validator): Update validator schema

--- src/validators/PIC.validator.js
+++ src/validators/PIC.validator.js
@@ -1,34 +1,67 @@
-const createPIC = {
-    type: 'object',
-    properties: {
-        name: { type: 'string', minLength: 1, maxLength: 68, nullable: false },
-        phone: { type: 'string', minLength: 7, maxLength: 13, nullable: false },
-        role: { type: 'string', enum: ["LIRA", "external", "midfielder"], nullable: false },
-    },
-    required: ['name', 'phone', 'role'],
-    additionalProperties: false,
+const Ajv = require('ajv');
+const ajv = new Ajv({ allErrors: true });
+
+require('ajv-errors')(ajv);
+
+ajv.addFormat("date", (data) => {
+    // Regular expression for yyyy-mm-dd format
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    return dateRegex.test(data);
+});
+
+function isCreatePICValid() {
+    const schema = {
+        type: 'object',
+        properties: {
+            name: { type: 'string', minLength: 1, maxLength: 68 },
+            phone: { type: 'string', minLength: 7, maxLength: 13 },
+            role: { type: 'string', enum: ['LIRA', 'external', 'midfielder'] },
+        },
+        required: ['name', 'phone', 'role'],
+        additionalProperties: false,
+    };
+
+    const validate = ajv.compile(schema);
+
+    return validate;
 };
 
-const updatePIC = {
-    type: 'object',
-    properties: {
-        name: { type: 'string', minLength: 1, maxLength: 68, nullable: false },
-        phone: { type: 'string', minLength: 7, maxLength: 13, nullable: false },
-        role: { type: 'string', enum: ["LIRA", "external", "midfielder"], nullable: false },
-    },
-    additionalProperties: false,
+function isUpdatePICValid() {
+    const schema = {
+        type: 'object',
+        properties: {
+            name: { type: 'string', minLength: 1, maxLength: 68 },
+            phone: { type: 'string', minLength: 7, maxLength: 13 },
+            role: { type: 'string', enum: ['LIRA', 'external', 'midfielder'] },
+        },
+        additionalProperties: false,
+    };
+
+    const validate = ajv.compile(schema);
+
+    return validate;
 };
 
-const getAllPICs = {
-    type: 'object',
-    properties: {
-        role: { type: 'string', enum: ["LIRA", "external", "midfielder"], nullable: false },
-    },
-    additionalProperties: true,
+function isGetAllPICValid() {
+    const dateErrorMessage = 'format should be \'yyyy-mm-dd\'';
+
+    const schema = {
+        type: 'object',
+        properties: {
+            role: { type: 'string', enum: ['LIRA', 'external', 'midfielder'] },
+            start_date: { type: 'string', format: 'date', errorMessage: dateErrorMessage },
+            end_date: { type: 'string', format: 'date', errorMessage: dateErrorMessage }
+        },
+        additionalProperties: true,
+    };
+
+    const validate = ajv.compile(schema);
+
+    return validate;
 };
 
 module.exports = {
-    createPIC,
-    updatePIC,
-    getAllPICs
+    isCreatePICValid,
+    isUpdatePICValid,
+    isGetAllPICValid
 };
