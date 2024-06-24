6949f7b7da7c4446ad9775513479d5e37d9ecd1c
Ivan Munandar <3332190035@untirta.ac.id>
2024-06-24 13:51:05 +0700

    Update Calculation after integrated schema

--- src/validators/Calculation.validator.js
+++ src/validators/Calculation.validator.js
@@ -1,41 +1,48 @@
-// const updateDataCalculation = {
+const updateDataCalculation = {
+    type: 'object',
+    properties: {
+        name: { type: 'string', nullable: true },
+        target_sales: { type: 'number', nullable: true },
+        business_model: { type: 'string', enum: [
+            'Subscription',
+            'OneTimeCharge'
+        ], nullable: true },
+        payback_period: { type: 'number', nullable: true },
+        operational_time: { type: 'number', nullable: true },
+        excess_capacity: { type: 'number', nullable: true },
+        unit_target: { type: 'string', enum: [
+            'User',
+            'Unit',
+            'Project'
+        ], nullable: true },
+        tariff: { type: 'number', nullable: true }
+    },
+    additionalProperties: false
+};
+
+module.exports = {
+    updateDataCalculation
+};
 //     type: 'object',
 //     properties: {
-//         targetSales: { 
-//             type: 'number', 
-//             nullable: true,
-//             minimum: 0 
-//         },
-//         paybackPeriod: { 
-//             type: 'number', 
-//             nullable: true,
-//             minimum: 0 
-//         },
-//         operationalTime: { 
-//             type: 'number', 
-//             nullable: true,
-//             minimum: 0 
-//         },
-//         excessCapacity: { 
-//             type: 'number', 
-//             nullable: true,
-//             minimum: 0 
-//         },
-//         businessModel: {
-//             type: 'string',
-//             enum: ['Subscription', 'OneTimeCharge'],
-//             nullable: true
-//         },
-//         unitTarget: {
-//             type: 'string',
-//             enum: ['User', 'Unit', 'Project'],
-//             nullable: true
-//         },
-//         tariff:{ 
-//             type: 'number', 
-//             nullable: true,
-//             minimum: 0 
-//         },
+//         name: { type: 'string', nullable: true },
+//         targetSales: { type: 'number',  nullable: true },
+//         businessModel: { type: 'string', enum: [
+//             'Subscription',
+//             'Pay-Per-Use',
+//             'Freemium',
+//             'One-Time Purchase',
+//             'One Time Charge',
+//         ], nullable: true },
+//         paybackPeriod: { type: 'number', nullable: true },
+//         operationalTime: { type: 'number', nullable: true },
+//         excessCapacity: { type: 'number', nullable: true },
+//         unitTarget: { type: 'string', enum: [
+//             'User',
+//             'Unit',
+//             'Project',
+//         ], nullable: true },
+//         tariff: { type: 'number', nullable: true },
 //     },
 //     additionalProperties: false,
 // }
@@ -43,31 +50,3 @@
 // module.exports = {
 //     updateDataCalculation,
 // };
-const updateDataCalculation = {
-    type: 'object',
-    properties: {
-        name: { type: 'string', nullable: true },
-        targetSales: { type: 'number',  nullable: true },
-        businessModel: { type: 'string', enum: [
-            'Subscription',
-            'Pay-Per-Use',
-            'Freemium',
-            'One-Time Purchase',
-            'One Time Charge',
-        ], nullable: true },
-        paybackPeriod: { type: 'number', nullable: true },
-        operationalTime: { type: 'number', nullable: true },
-        excessCapacity: { type: 'number', nullable: true },
-        unitTarget: { type: 'string', enum: [
-            'User',
-            'Unit',
-            'Project',
-        ], nullable: true },
-        tariff: { type: 'number', nullable: true },
-    },
-    additionalProperties: false,
-}
-
-module.exports = {
-    updateDataCalculation,
-};
