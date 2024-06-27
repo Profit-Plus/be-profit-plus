461de9fa6f94f82bd5508fd7078b455882d81d80
Ivan Munandar <3332190035@untirta.ac.id>
2024-06-27 09:53:32 +0700

    update calculation validator

--- src/validators/Calculation.validator.js
+++ src/validators/Calculation.validator.js
@@ -5,7 +5,10 @@ const updateDataCalculation = {
         target_sales: { type: 'number', nullable: true },
         business_model: { type: 'string', enum: [
             'Subscription',
-            'OneTimeCharge'
+            'OneTimeCharge',
+            'Freemium',
+            'Licensing',
+            'Pay_per_Use',
         ], nullable: true },
         payback_period: { type: 'number', nullable: true },
         operational_time: { type: 'number', nullable: true },
@@ -15,6 +18,9 @@ const updateDataCalculation = {
             'Unit',
             'Project'
         ], nullable: true },
+        capex: { type: 'number', nullable: true },
+        opex: { type: 'number', nullable: true },
+        cogs: { type: 'number', nullable: true },
         tariff: { type: 'number', nullable: true }
     },
     additionalProperties: false
@@ -23,30 +29,3 @@ const updateDataCalculation = {
 module.exports = {
     updateDataCalculation
 };
-//     type: 'object',
-//     properties: {
-//         name: { type: 'string', nullable: true },
-//         targetSales: { type: 'number',  nullable: true },
-//         businessModel: { type: 'string', enum: [
-//             'Subscription',
-//             'Pay-Per-Use',
-//             'Freemium',
-//             'One-Time Purchase',
-//             'One Time Charge',
-//         ], nullable: true },
-//         paybackPeriod: { type: 'number', nullable: true },
-//         operationalTime: { type: 'number', nullable: true },
-//         excessCapacity: { type: 'number', nullable: true },
-//         unitTarget: { type: 'string', enum: [
-//             'User',
-//             'Unit',
-//             'Project',
-//         ], nullable: true },
-//         tariff: { type: 'number', nullable: true },
-//     },
-//     additionalProperties: false,
-// }
-
-// module.exports = {
-//     updateDataCalculation,
-// };
